85ec02a593c880ac5aea8121a37dae363412180a
elloveronika <ello.veronika92@gmail.com>
2022-03-01 23:10:38 -0800

    added first platform, need to fix y axis bug

--- script.js
+++ script.js
@@ -23,21 +23,22 @@ class Player {
     //the y velocity will be pushing our player down
     this.velocity = {
       x: 0,
-      y: 1,
+      y: 0,
     };
     //we need a width and height in order to create a square
-    this.width = 20;
-    this.height = 20;
+    this.width = 30;
+    this.height = 30;
   }
   //an arbitrary method, this is where we will draw out our player
   draw() {
-    ctx.fillStyle = "red";
+    ctx.fillStyle = "purple";
     //we are defining what our player is actually going to look like
     //we are calling our canvas method fillrect, we are referncing the above positionto fill in
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
     if (this.position.y + this.height + this.velocity.y <= canvas.height) {
       this.velocity.y += gravity;
@@ -46,11 +47,35 @@ class Player {
     }
   }
 }
+class Platform {
+  constructor() {
+    this.position = {
+      x: 400,
+      y: 400,
+    };
+    this.width = 200;
+    this.height = 20;
+  }
+  draw() {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
 
 //here we implement a new player
 const player = new Player();
+const platform = new Platform();
 //here we calll the draw method on the player
 player.update();
+
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+};
 
 function animate() {
   //calling this recursive loop so it can keep calling animate over and over again over time
@@ -59,13 +84,59 @@ function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   player.update();
+  platform.draw();
+
+  keys.right.pressed ? (player.position.x += 5) : (player.velocity.x = 0);
+  keys.left.pressed ? (player.velocity.x = -5) : (player.velocity.x = 0);
+
+  if (
+    player.position.y + player.height <= platform.position.y &&
+    player.position.y + player.height + player.velocity.y >= platform.position.y
+  ) {
+    player.velocity.y = 0;
+  }else{
+    
+  }
 }
 animate();
 
 window.addEventListener("keydown", ({ keyCode }) => {
-  console.log(keyCode);
-//   switch (keyCode) {
-//     case: 65
-        
-//   }
+  // console.log(keyCode);
+  switch (keyCode) {
+    case 65:
+      console.log("a: left");
+      keys.left.pressed = true;
+      break;
+    case 87:
+      console.log("w: up");
+      player.position.y -= 400;
+      break;
+    case 83:
+      console.log("s: down");
+      break;
+    case 68:
+      console.log("d: right");
+      keys.right.pressed = true;
+      break;
+  }
+});
+
+window.addEventListener("keyup", ({ keyCode }) => {
+  // console.log(keyCode);
+  switch (keyCode) {
+    case 65:
+      console.log("a: left");
+      keys.left.pressed = false;
+      break;
+    case 87:
+      console.log("w: up");
+      break;
+    case 83:
+      console.log("s: down");
+      break;
+    case 68:
+      console.log("d: right");
+      keys.right.pressed = false;
+      break;
+  }
 });
